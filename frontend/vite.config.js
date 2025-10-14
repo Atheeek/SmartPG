c63083aad64e45ae7c4b50cb2ff569c85889ee8b
Atheeek <atheek163@gmail.com>
2025-10-14 13:12:05 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: './', // ✅ ensures relative asset paths (important for Vercel)
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
